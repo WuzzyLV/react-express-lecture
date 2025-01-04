d18f2ad48493246da72cbc93ee20d49064e4a7ba
WuzzyLV <WuzzyLV@users.noreply.github.com>
2025-01-04 12:01:32 +0200

    remove random

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,6 @@ app.use(bodyParser.json());
 mongoose
   .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
-    useUnifiedTopology: true,
   })
   .then(() => {
     console.log("MongoDB connected");
