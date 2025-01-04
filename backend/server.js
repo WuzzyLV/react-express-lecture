1c77efd7adb3df232fb64067df2b63a7880c1fcb
WuzzyLV <WuzzyLV@users.noreply.github.com>
2025-01-04 11:24:47 +0200

    proxy for backend

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,9 @@
 import express from "express";
 import cors from "cors";
 import bodyParser from "body-parser";
-import taskRoutes from "./routes/tasks.js";
 
 const app = express();
-const PORT = 5000;
+const PORT = 3000;
 
 app.use(cors());
 app.use(bodyParser.json());
