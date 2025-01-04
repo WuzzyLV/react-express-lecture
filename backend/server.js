65f9519bb30404703ba9738e1faa0b23dbdb41ac
WuzzyLV <WuzzyLV@users.noreply.github.com>
2025-01-04 11:55:03 +0200

    nodemon

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,33 @@
 import express from "express";
 import cors from "cors";
 import bodyParser from "body-parser";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+
+dotenv.config(); // Load environment variables from .env file
 
 const app = express();
 const PORT = 3000;
 
 app.use(cors());
 app.use(bodyParser.json());
+
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("MongoDB connected");
+  })
+  .catch((err) => {
+    console.error("MongoDB connection error:", err);
+  });
+
 app.use("/hello", (req, res) => {
   res.send("Hello World");
 });
 
-app.listen(PORT, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log(`Server running at http://localhost:${PORT}`);
 });
