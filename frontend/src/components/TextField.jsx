e42f9b3dcd60853f084fffc7156f1346cf27cc46
WuzzyLV <WuzzyLV@users.noreply.github.com>
2025-01-05 14:59:30 +0200

    add Button comp

--- frontend/src/components/TextField.jsx
+++ frontend/src/components/TextField.jsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import { Button } from "./Button";
 
 export const TextField = () => {
   const [title, setTitle] = useState(null);
@@ -17,53 +18,33 @@ export const TextField = () => {
       title,
       message,
     };
-    fetch('http://localhost:5173/api/messages', {
-      method: 'POST',
+    fetch("http://localhost:5173/api/messages", {
+      method: "POST",
       body: JSON.stringify(data),
       headers: {
-        'Content-Type': 'application/json; charset=UTF-8',
+        "Content-Type": "application/json; charset=UTF-8",
       },
     }).then((data) => console.log(data));
   };
 
   return (
-    <section className='max-w-3xl border-[#999999] border mx-auto mt-20 text-white'>
-      <form className='flex flex-col p-4 gap-4' onSubmit={handleSubmit}>
-        <div className='relative border border-[#999999] p-2'>
-          <label
-            htmlFor='name'
-            className='absolute -top-3 left-2 bg-black px-1 text-lg font-bold'
-          >
+    <section className="max-w-3xl border-[#999999] border mx-auto mt-20 text-white">
+      <form className="flex flex-col p-4 gap-4" onSubmit={handleSubmit}>
+        <div className="relative border border-[#999999] p-2">
+          <label htmlFor="name" className="absolute -top-3 left-2 bg-black px-1 text-lg font-bold">
             Title
           </label>
-          <input
-            type='text'
-            id='title'
-            name='title'
-            className='w-full border-none outline-none bg-black p-2'
-            onChange={handleTitleChange}
-          />
+          <input type="text" id="title" name="title" className="w-full border-none outline-none bg-black p-2" onChange={handleTitleChange} />
         </div>
-        <div className='relative border border-[#999999] p-2 '>
-          <label
-            htmlFor='message'
-            className='absolute -top-3 left-2 bg-black px-1 text-lg font-bold'
-          >
+        <div className="relative border border-[#999999] p-2 ">
+          <label htmlFor="message" className="absolute -top-3 left-2 bg-black px-1 text-lg font-bold">
             Message
           </label>
-          <textarea
-            id='message'
-            name='message'
-            className='w-full p-2 border-none outline-none bg-black max-h-[100px]'
-            onChange={handleMessageChange}
-          />
+          <textarea id="message" name="message" className="w-full p-2 border-none outline-none bg-black max-h-[100px]" onChange={handleMessageChange} />
         </div>
-        <button
-          type='submit'
-          className='bg-[#9999] text-white font-bold py-2 px-4  mt-4'
-        >
+        <Button disabled={true} type={"submit"}>
           Submit
-        </button>
+        </Button>
       </form>
     </section>
   );
