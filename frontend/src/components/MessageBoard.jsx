7b1a4fb23458d75a15919ee0a7fad066fb9726c2
lincis <linardssils1@gmail.com>
2025-01-05 16:28:49 +0200

    feat: Add MessageBoard component for displaying messages

---
+++ frontend/src/components/MessageBoard.jsx
@@ -0,0 +1,24 @@
+import { useContext } from 'react';
+import { MessageCard } from './MessageCard';
+import { MessageContext } from '../context/useMessages';
+
+export const MessageBoard = () => {
+  const { messages } = useContext(MessageContext);
+
+  return (
+    <div className='max-w-3xl text-white mx-auto mt-20'>
+      <div className='flex flex-col gap-4'>
+        {messages &&
+          messages.map((message) => {
+            return (
+              <MessageCard
+                key={message._id}
+                title={message.title}
+                message={message.message}
+              />
+            );
+          })}
+      </div>
+    </div>
+  );
+};
