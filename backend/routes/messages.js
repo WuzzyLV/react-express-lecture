39c54919e88a813de098f6ebdb1a8543f740d297
WuzzyLV <WuzzyLV@users.noreply.github.com>
2025-01-05 13:23:09 +0200

    fix endpoint

--- backend/routes/messages.js
+++ backend/routes/messages.js
@@ -7,8 +7,13 @@ router.get("/", async (req, res) => {
   res.send(messages);
 });
 
-router.post("/messages", async (req, res) => {
+router.post("/", async (req, res) => {
   const { username, message, ip } = req.body;
+  const oneHourAgo = new Date(Date.now() - 60 * 60 * 1000);
+  const existingMessage = await MessageSchema.findOne({ ip, createdAt: { $gte: oneHourAgo } });
+  if (existingMessage) {
+    return res.status(400).send({ error: "A message from this IP address already exists within the last hour." });
+  }
   const newMessage = new MessageSchema({
     username,
     message,
