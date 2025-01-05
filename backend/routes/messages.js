da5425e50c07b84a5a717792d05eb2b156312748
WuzzyLV <WuzzyLV@users.noreply.github.com>
2025-01-05 13:27:59 +0200

    get ip proper

--- backend/routes/messages.js
+++ backend/routes/messages.js
@@ -1,14 +1,15 @@
-import express from 'express';
-import MessageSchema from '../schemas/MessageSchema.js';
+import express from "express";
+import MessageSchema from "../schemas/MessageSchema.js";
 const router = express.Router();
 
-router.get('/', async (req, res) => {
+router.get("/", async (req, res) => {
   const messages = await MessageSchema.find();
   res.send(messages);
 });
 
-router.post('/', async (req, res) => {
-  const { title, message, ip } = req.body;
+router.post("/", async (req, res) => {
+  const { title, message } = req.body;
+  ip = req.headers["x-forwarded-for"] || req.connection.remoteAddress;
   const oneHourAgo = new Date(Date.now() - 60 * 60 * 1000);
   const existingMessage = await MessageSchema.findOne({
     ip,
@@ -16,8 +17,8 @@ router.post('/', async (req, res) => {
   });
   if (existingMessage) {
     return res.status(400).send({
-      error:
-        'A message from this IP address already exists within the last hour.',
+      error: "A message from this IP address already exists within the last hour.",
+      timeToWait: Math.ceil((existingMessage.createdAt - new Date() + 60 * 60 * 1000) / 1000),
     });
   }
   const newMessage = new MessageSchema({
