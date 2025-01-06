633125c08c72b6ce1309b3702adca6f60b74990e
WuzzyLV <WuzzyLV@users.noreply.github.com>
2025-01-06 10:12:59 +0200

    improve design

--- frontend/src/components/MessageCard.jsx
+++ frontend/src/components/MessageCard.jsx
@@ -1,20 +1,22 @@
-import React from 'react';
+import React from "react";
 
-export const MessageCard = ({ title, message }) => {
+export const MessageCard = ({ title, message, createdAt }) => {
   return (
-    <div className='border-[#999999] border flex flex-col p-4 gap-2'>
+    <div className="border-[#999999] border flex flex-col p-4 gap-2 relative">
       <div>
-        <span className='flex gap-2'>
-          <p className='font-bold'>Title: </p>
-          <p>{title || ''}</p>
+        <span className="flex gap-2 text-accent">
+          {/* <p className="font-bold">Title: </p> */}
+          <p className="text-2xl font-extrabold">{title || ""}</p>
         </span>
       </div>
       <div>
-        <span className='flex gap-2'>
-          <p className='font-bold'>Message: </p>
-          <p>{message || ''}</p>
+        <span className="flex gap-2 text-xl">
+          <p>{message || ""}</p>
         </span>
       </div>
+      <span className="flex gap-2 absolute -bottom-3 left-2 bg-black text-[#999999] px-2">
+        <p>{new Date(createdAt).toLocaleString() || ""}</p>
+      </span>
     </div>
   );
 };
