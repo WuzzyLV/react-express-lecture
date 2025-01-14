b0d0579adc0ac3260a73789bd75e4f91a619bed9
lincis <linardssils1@gmail.com>
2025-01-14 18:30:48 +0200

    feat: Optimize MessageCard component and implement voting functionality

--- frontend/src/components/MessageCard.jsx
+++ frontend/src/components/MessageCard.jsx
@@ -1,8 +1,31 @@
-import { Downvote } from './Downvote';
-import { Upvote } from './Upvote';
+import { useContext, useEffect, useState } from 'react';
 import { Voting } from './Voting';
+import { MessageContext } from '../context/useMessages';
 
-export const MessageCard = ({ title, message, createdAt }) => {
+export const MessageCard = ({
+  title,
+  message,
+  createdAt,
+  initialCount,
+  id,
+  userVote,
+}) => {
+  const [count, setCount] = useState(initialCount);
+
+  const { handleVote } = useContext(MessageContext);
+
+  const handleUpvote = () => {
+    handleVote(id, true);
+  };
+
+  const handleDownvote = () => {
+    handleVote(id, false);
+  };
+
+  useEffect(() => {
+    setCount(initialCount);
+  }, [initialCount]);
+
   return (
     <div className='border-[#999999] border flex flex-col p-4 gap-2 relative'>
       <div>
@@ -18,7 +41,13 @@ export const MessageCard = ({ title, message, createdAt }) => {
       <span className='flex gap-2 absolute -bottom-3 right-2 bg-black text-[#999999] px-2'>
         <p>{new Date(createdAt).toLocaleString() || ''}</p>
       </span>
-      <Voting count={100} />
+      <Voting
+        count={count}
+        id={id}
+        userVote={userVote}
+        onUpvote={handleUpvote}
+        onDownvote={handleDownvote}
+      />
     </div>
   );
 };
